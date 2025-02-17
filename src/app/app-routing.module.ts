2fc6436b7a6a9981d2fb74b3c73235fda1b99520
enrico-dgr <enrico.digrazia98@gmail.com>
2025-02-17 11:57:54 +0100

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,59 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./pages/home/home.module').then(m => m.HomeModule),
+  },
+  {
+    path: 'booking',
+    loadChildren: () =>
+      import('./pages/booking/booking.module').then(m => m.BookingModule),
+  },
+  {
+    path: 'contact',
+    loadChildren: () =>
+      import('./pages/contact/contact.module').then(m => m.ContactModule),
+  },
+  {
+    path: 'about',
+    loadChildren: () =>
+      import('./pages/about/about.module').then(m => m.AboutModule),
+  },
+  {
+    path: 'menu',
+    loadChildren: () =>
+      import('./pages/menu/menu.module').then(m => m.MenuModule),
+  },
+  {
+    path: 'our-team',
+    loadChildren: () =>
+      import('./pages/our-team/our-team.module').then(m => m.OurTeamModule),
+  },
+  {
+    path: 'service',
+    loadChildren: () =>
+      import('./pages/service/service.module').then(m => m.ServiceModule),
+  },
+  {
+    path: 'testimonial',
+    loadChildren: () =>
+      import('./pages/testimonial/testimonial.module').then(
+        m => m.TestimonialModule,
+      ),
+  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: '**',
+    loadChildren: () =>
+      import('./pages/not-found/not-found.module').then(m => m.NotFoundModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
